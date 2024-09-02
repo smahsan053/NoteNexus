f6c51637a5cba9b792a3c8929ff08af2d88c0d00
smahsan053 <smahsan053@gmail.com>
2024-09-02 22:00:28 +0500

    pre deployment changes

--- server/server.js
+++ server/server.js
@@ -5,16 +5,19 @@ import noteRoutes from "./routes/notes.js";
 import connectToMongo from "./config/db.js";
 import cors from "cors";
 
+
+
 dotenv.config();
+const port = process.env.PORT || 3000;
+const app = express();
 connectToMongo();
 
-const port = process.env.PORT;
-const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", noteRoutes);
+
 app.listen(port, () => {
   console.log(`NoteNexus backend listening on port ${port}`);
 });
