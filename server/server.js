6e69933d2d0a03271cb2f014c6df23fd735e1e81
smahsan053 <smahsan053@gmail.com>
2024-09-15 22:22:17 +0500

    cors policy updated

--- server/server.js
+++ server/server.js
@@ -5,15 +5,21 @@ import noteRoutes from "./routes/notes.js";
 import connectToMongo from "./config/db.js";
 import cors from "cors";
 
-
-
 dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
 connectToMongo();
 
-app.use(cors());
 app.use(express.json());
+// Use CORS and specify your frontend domain
+app.use(
+  cors({
+    origin:
+      "https://client-cfvt95jpp-syed-muhammad-ahsan-alis-projects.vercel.app", // Frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+    credentials: true, // If your requests include credentials like cookies
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", noteRoutes);
